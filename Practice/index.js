feabcf2eeb4825bb6922d3e08007025b144ae3fc
kibbang <007kibbang@gmail.com>
2020-07-24 17:59:00 +0900

    Your first JS Function

--- Practice/index.js
+++ Practice/index.js
@@ -54,7 +54,12 @@ set memory: ƒ ()
 // 1. 함수선언 function
 // 2. 함수 이름 선언 function ~~~
 // 3. 내용 입력
-function sayHello(name, age){  // 원하는 것을 적어주면 됨 아무거나 (coffee가 함수안에서 사용하게 될 이름이 됨)
+
+function sayHello(){  // 원하는 것을 적어주면 됨 아무거나 (coffee가 함수안에서 사용하게 될 이름이 됨)
+  console.log(" Hello "); // 여기에서 coffee를 사용해준다.
+}
+
+function sayHello2(name, age){  // 원하는 것을 적어주면 됨 아무거나 (coffee가 함수안에서 사용하게 될 이름이 됨)
   console.log(" Hello ", name, " Your age is ", age); // 여기에서 coffee를 사용해준다.
 }
 
@@ -64,13 +69,14 @@ sayHello();
 // console.log()의 log()도 함수임! console이라는 Object안에 있는 함수
 // log()는 메시지가 없으면 안돌아감
 // 함수의 () 안에 무엇인가를 넣으면 함수가 사용하게 됨
-sayHello("Bro!", 20); // 이렇게 실행하면 sayHello();와 똑같음
+sayHello("Bro!"); // 이렇게 실행하면 sayHello();와 똑같음
 
 // 왜 안되는가
 // ("Bro!")의 "Bro!"는 인자(argument)임
 // sayHello()는 인자를 받음. 인자(argument)는 변수 같은 것. 주는 값을 저장함
 // 인자를 가져가는 방법은 함수를 선언할 때 적어줌 57라인 참조
 // "Bro!"를 coffee에 넣고 그것을 사용하게 됨
+sayHello2("Bro!", 20);
 // 이것이 외부에 있는 데이터를 읽는 함수를 만드는 방법
 // console.log()는 인자(argument)를 무한히 가질 수 있고 뭘 넣든 다 할 수 있다.
 // sayHello()는 2개의 인자(argument)를 가짐.
