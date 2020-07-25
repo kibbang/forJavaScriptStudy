e9f4399639d537d1f3918f4ead23654973e2e2aa
kibbang <007kibbang@gmail.com>
2020-07-26 00:44:16 +0900

    put In background Image

---
+++ momentum/background.js
@@ -0,0 +1,23 @@
+// Put In Background Image //
+
+const body = document.querySelector("body");
+const IMG_NUMBER = 4;
+
+
+function paintImage(imageNumber){
+  const image = new Image();
+  image.src = `images/${imageNumber + 1}.jpg`;
+  image.classList.add("backgroundImage");
+  body.prepend(image);
+}
+
+function genRandom(){
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function init(){
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
+}
+init();
