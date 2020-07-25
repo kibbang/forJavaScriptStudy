dddaf4e88887237755005c5daa3ad75ea18cd150
kibbang <007kibbang@gmail.com>
2020-07-25 15:52:14 +0900

    Making a JS Clock part 2

--- momentum/clock.js
+++ momentum/clock.js
@@ -9,10 +9,24 @@ function getTime(){
   const hours = date.getHours();
   const seconds = date.getSeconds();
 
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init(){
   getTime();
+  setInterval(getTime, 1000);
 }
 init();
+
+// 초를 매번 업데이트 하기 위해 setInterval() 이란 함수를 쓴다.
+// setInterval(fuction, 인자로 받은 함수를 실행하고 싶은 시간(실행할 간격) ms단위)
+// {
+//    block;
+// }
+
+// seconds를 01,02 이렇게 표시하는 법
+// 삼항 연산자 사용
+// ? -> if
+// : -> else
